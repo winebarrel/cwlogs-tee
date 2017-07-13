82cdf6d37aee1ae19bb6585cfd755b8d3898bfb8
Genki Sugawara <sugawara@cookpad.com>
2017-07-13 15:46:36 +0900

    Fix brew formula

--- homebrew/cwlogs-tee.rb
+++ homebrew/cwlogs-tee.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class CwlogsTee < Formula
-  VERSION = '0.1.2'
+  VERSION = '0.1.3'
 
   homepage 'https://github.com/winebarrel/cwlogs-tee'
   url "https://github.com/winebarrel/cwlogs-tee/releases/download/v#{VERSION}/cwlogs-tee-v#{VERSION}-darwin-amd64.gz"
-  sha256 'e4c6943a3ee8b49c9a4f40b287ac13dac267215611658d4aabe0a6cfabc0fe39'
+  sha256 '0c9441b883724327c79072fe19cc88e69591e2f73c3210249335fd5c14bb5eb8'
   version VERSION
   head 'https://github.com/winebarrel/cwlogs-tee.git', :branch => 'master'
 
